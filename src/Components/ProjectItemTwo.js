a6056c9024fd87edd0964df3dfe9b1b8a06f00ea
Jeff Jiang <jeffjiang8@gmail.com>
2020-02-14 16:17:22 -0500

    projects 1/3 done

--- src/Components/ProjectItemTwo.js
+++ src/Components/ProjectItemTwo.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class ProjectItemTwo extends Component {
+
+    state = {
+        closed: true
+    }
+
+    handleHover = ()=>{
+        console.log('hovering')
+        this.setState({
+            closed: !this.state.closed
+        })
+    }
     render() {
         return (
-            <div className="project-two">
-                <p>Project Two</p>
+            <div className="project-two" onMouseEnter={this.handleHover} onMouseLeave={this.handleHover}>
+                <div className={this.state.closed?`overlay`:`overlay-open`}>
+                    <p className={this.state.closed?`frogger-desc-closed`:`frogger-desc`}>
+                        This is a clone of the classic game named Frogger.<br></br>
+                        This collaborative project is composed with JavaScript.<br></br>
+                    </p>
+                    <div className="frogger-links">
+                        <a href="https://github.com/maryfarner18/critter-crossing-frontend" 
+                            target="_blank"
+                            className={this.state.closed?"frogger-code-closed":"frogger-code"}>
+                            [Code]
+                        </a>
+                        &nbsp;
+                        <a href="https://www.youtube.com/watch?v=Zkm7kYnViZc&feature=youtu.be" 
+                            target="_blank"
+                            className={this.state.closed?"frogger-code-closed":"frogger-code"}>
+                            [Demo]
+                        </a>
+                    </div>
+                </div>
+                <img src='http://localhost:3000/images/frogger.gif' alt='meh' className="frogger-img"/>
             </div>
         );
     }
