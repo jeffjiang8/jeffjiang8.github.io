74cb785b5945a9fa76cfca78c09521861a0d27b0
Jeff Jiang <jeffjiang8@gmail.com>
2020-02-20 15:50:12 -0500

    DONE:

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,9 +6,9 @@ class Home extends Component {
             <div className={this.props.view?`home slider`:`home slider closed`}>
                 <h1 className="header">I'm Jinfeng Jiang.</h1>
                 <div className="home-desc">
-                    <p style={{ 'font-weight': '500', 'color':'rgb(166, 166, 166)'}}><span style={{'text-decoration':'underline', 'color':'white'}}>Full-Stack Software Engineer</span> skilled in Ruby on Rails, JavaScript, and React.js.</p> 
-                    <p style={{ 'font-weight': '500', 'color':'rgb(166, 166, 166)'}}>Adaptive and detail oriented individual, background in <span style={{'text-decoration':'underline', 'color':'white'}}>Hardware Engineering</span>.</p>
-                    <p style={{ 'font-weight': '500', 'color':'rgb(166, 166, 166)'}}>Thrive in both collaborative and independent environment.</p>
+                    <p style={{ 'font-weight': '500', 'color':'rgb(166, 166, 166)', 'font-size': "22px"}}><span style={{'text-decoration':'underline', 'color':'white'}}>Full-Stack Software Engineer</span> skilled in Ruby on Rails, JavaScript, and React.js.</p> 
+                    <p style={{ 'font-weight': '500', 'color':'rgb(166, 166, 166)', 'font-size': "22px"}}>Adaptive and detail oriented individual, background in <span style={{'text-decoration':'underline', 'color':'white'}}>Hardware Engineering</span>.</p>
+                    <p style={{ 'font-weight': '500', 'color':'rgb(166, 166, 166)', 'font-size': "22px"}}>Thrive in both <span style={{'text-decoration':'underline', 'color':'white'}}>collaborative</span> and <span style={{'text-decoration':'underline', 'color':'white'}}>independent</span> environment.</p>
                 </div>
             </div>
         );
